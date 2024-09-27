61bc3f2e2dc77fc2339b8ee5acbee09deddcb587
yoshitha05 <142377316+yoshitha05@users.noreply.github.com>
2024-09-27 13:50:31 +0530

    Create FunctionalComponent.js

---
+++ Components/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FunctionalComponent() {
+  return (
+    <div>
+      <h1>This is Functional Component</h1>
+    </div>
+  )
+}
+
+export default FunctionalComponent
