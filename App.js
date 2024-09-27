b8f3e150a8a8e319845f26f3062fefc461c7a3da
yoshitha05 <142377316+yoshitha05@users.noreply.github.com>
2024-09-27 13:44:12 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import FunctionalComponent from './Components/FunctionalComponent';
+import ClassComponent from './Components/ClassComponent';
+import Hello from './Components/Hello';
+import Messsage from './Components/Message';
+function App() {
+  return (
+    <div className="App">
+      <FunctionalComponent />
+      <ClassComponent />
+      <Hello name = 'susruthi' lastname = 'kanaparthi'/>
+      <Messsage />
+    </div>
+  );}
+export default App;
